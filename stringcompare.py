8ace1d7825ee9afe6cc3ff2cfcbcae9cdbad4d39
rakeshnayak19 <161328225+rakeshnayak19@users.noreply.github.com>
2024-03-11 15:03:53 +0530

    Add files via upload

---
+++ stringcompare.py
@@ -0,0 +1,43 @@
+#1.String copy
+#2. String compare
+#3. String concatate
+#4. slicing
+
+str1="cmr"
+str2="engineering college"
+print(str1)
+print(str2)
+str3=str2
+print(str3)
+str4=str1+" "+str2
+print(str4)
+x=str1==str2
+print(x)
+x=str3==str2
+print(x)
+str='cmr engineering college'
+k=str[0:3]
+print(k)
+k=str[:7]
+print(k)
+k=str[1:7]
+print(k)
+
+
+#mutable
+a=[1,2,3.5,"abs"]
+print(a)
+a[1]=10
+print(a)
+k=a[1:3]
+print(k)
+k=a[2:]
+print(k)
+k=a[:2]
+print(k)
+k=a[-1]
+print(k)
+k=a[-1:-4]
+print(k)
+
+
